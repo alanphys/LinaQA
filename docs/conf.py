cd4d8959cd8355a3e9f783bff5da0be09be7318a
alanphys <alanphys@yahoo.co.uk>
2025-09-17 11:04:08 +0200

    Add placeholder for documentation

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'LinaQA'
+copyright = '2025, AC Chamberlain'
+author = 'AC Chamberlain'
+release = '1.00'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,14 @@
+.. LinaQA documentation master file, created by
+   sphinx-quickstart on Wed Sep 17 10:44:05 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to LinaQA's documentation
+=================================
+
+Place holder for documentation
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
