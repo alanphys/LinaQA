8a9506f96273f46658354a825e4cd3cf40d8841b
alanphys <alanphys@yahoo.co.uk>
2026-03-19 14:31:07 +0200

    Delete unused imports from settingsunit

--- aboutpackage/aboutform.py
+++ aboutpackage/aboutform.py
@@ -13,7 +13,7 @@ from .aboutformui import Ui_AboutForm
 from PyQt5.QtWidgets import QDialog
 import os
 
-version = "0.08.561"  # previous git commit 71644108
+version = "0.08.562"  # previous git commit dafda8c7
 
 
 class About(QDialog):
