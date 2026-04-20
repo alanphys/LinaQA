94ab12fea81c0a4b4c023a7764d184128298801d
alanphys <alanphys@yahoo.co.uk>
2026-04-20 08:35:11 +0200

    Add SUV Uptake to menu

--- aboutpackage/aboutform.py
+++ aboutpackage/aboutform.py
@@ -13,7 +13,7 @@ from .aboutformui import Ui_AboutForm
 from PyQt5.QtWidgets import QDialog
 import os
 
-version = "0.08.591"  # previous git commit 2765b26e
+version = "0.08.592"  # previous git commit bb7e13dc
 
 
 class About(QDialog):
